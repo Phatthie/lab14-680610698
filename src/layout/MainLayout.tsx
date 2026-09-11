aa758885a1346f642bf6763744400c9eec76dc53
Phatthie <younefayya@gmail.com>
2026-09-11 13:37:15 +0700

    sss

--- src/layout/MainLayout.tsx
+++ src/layout/MainLayout.tsx
@@ -20,7 +20,7 @@ export default function MainLayout() {
           </main>
 
           {/* Footer ด้านล่าง */}
-          <Footer year="2026" fullName="ชื่อ-สกุล" studentId="รหัสนักศึกษา" />
+          <Footer year="2026" fullName="Phatthira Rojanaphiboontham" studentId="680610698" />
         </div>
       </div>
     </div>
